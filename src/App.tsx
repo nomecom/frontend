0b3968c5734548a2016b09a233803edade1425bb
Dhruvwalia05 <dhruvwalia343@gmail.com>
2025-08-05 15:40:38 +0530

    Login Signup Page Frontend done

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import LoginSignup from './LoginSignup/LoginSignup.jsx';
+import './App.css'
+function App() {
+  return (
+    <div className="App">
+      <LoginSignup />
+    </div>
+  );
+}
+
+export default App;
