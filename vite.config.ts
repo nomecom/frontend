ac497d2abb1c6382a4741c1c9f2ecceb118db7f5
Dhruvwalia05 <dhruvwalia343@gmail.com>
2025-08-08 03:06:57 +0530

    deploy changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/hello': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-      },
-    },
-  },
+  base: "/", // <--- Make sure this is set properly
 })
